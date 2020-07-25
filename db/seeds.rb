c13fdb96dc1bf4ba274de57ecf199f39fe465edc
Kristin Howard <hkristin@andrew.cmu.edu>
2020-07-25 19:54:10 +0000

    created seed data to test with

---
+++ db/seeds.rb
@@ -0,0 +1,21 @@
+brandon = User.new(
+  id: 1,
+  username: "Brandon",
+  email: "brandon@example",
+  password: "password"
+  )
+  
+brandon.save
+  
+brandon_brand_1 = Brand.new(
+  id: 1,
+  name: "Tarte",
+  palettes: "Beach Babe",
+  age: 10, 
+  clean: true,
+  user_id: brandon.id
+  )
+  
+
+  brandon_brand_1.save
+  
