af08e2840423628e209fc92f6bc8a149212e2c9c
Kristin Howard <hkristin@andrew.cmu.edu>
2020-08-04 16:59:04 +0000

    Added validations for user input

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
   has_secure_password
   has_many :brands
+
+  validates :email, presence: true
+  validates :email, uniqueness : { message: "%{value} is already spoken for. Please try again with a different email."}
 end
